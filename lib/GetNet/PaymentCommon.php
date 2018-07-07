36c439ecb8c6c44b4250ebd4e7d3cc3c72f3c8b2
victorcaina <victor.caina@hotmail.com>
2018-07-07 17:05:27 -0300

    Payment bank slip

---
+++ lib/GetNet/PaymentCommon.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *
+ */
+class GetNet_PaymentCommon extends GetNet_CardHashCommon
+{
+	public function __construct($response = array())
+	{
+		parent::__construct($response);
+	}
+
+	public function create($path)
+	{
+		parent::create($path);
+	}
+}
